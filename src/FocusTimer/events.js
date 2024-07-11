14a36db1a6947bdfa41ec860b3d4ddd69dcb3620
Victor Zornek <vzornek@gmail.com>
2024-07-10 23:36:00 -0300

    finalizando a lógica do timer e o display

--- src/FocusTimer/events.js
+++ src/FocusTimer/events.js
@@ -1,5 +1,8 @@
+import state from "./state.js"
 import { controls } from "./elements.js"
 import * as actions from "./actions.js"
+import * as el from "./elements.js"
+import { updateDisplay } from "./timer.js"
 
 // function que registra tudo que está sendo clicado dentro dos controls
 export function registerControls() {
@@ -11,5 +14,24 @@ export function registerControls() {
         }
 
         actions[action]()
+    })
+}
+
+export function setMinutes() {
+    el.minutes.addEventListener('focus', () => {
+        el.minutes.textContent = ""
+    })
+
+    el.minutes.onkeypress = (event) => /\d/.test(event.key)   // uso de uma expressão regular para bloquear a digitação de letras nos minutos´
+
+    el.minutes.addEventListener('blur', (event) => {
+        let time = event.currentTarget.textContent           // fora do foco verifica se o número digitado é maior que 60, se for, retorna para 60
+        time = time > 60 ? 60 : time
+
+        state.minutes = time
+        state.seconds = 0
+
+        updateDisplay()
+        el.minutes.removeAttribute('contenteditable')
     })
 }
